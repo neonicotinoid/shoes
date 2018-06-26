dbf14475d33701d93df874e361081bc07613803a
neonicotinoid <skyyyr@outlook.com>
2018-06-26 13:58:10 +0300

    initial

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+console.log('ok');
+
+ 
+var ham = document.querySelector('.menu-controller'),
+    menu = document.querySelector('.main-menu');
+
+ham.addEventListener( 'click' , function() {
+  menu.classList.toggle('is-open');
+});
+
+$('.slider-wrapper').slick({
+  dots: true,
+  infinite: true,
+  autoplay: true,
+  arrows: false,
+  autoplaySpeed: 3000,
+  pauseOnFocus: true,
+  customPaging : function(slider, i) {
+    var title = $(slider.$slides[i]).data('title'),
+        desc = $(slider.$slides[i]).data('desc'); 
+    return '<div class="dot__text"><span class="dot__name">' + title + '</span><span class="dot__desc">' +  desc  + '</span></div>';
+},
+responsive: [
+  {
+    breakpoint: 768,
+    settings: {
+      infinite: true,
+      arrows: false,
+    }
+  },
+  // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+]
+});
